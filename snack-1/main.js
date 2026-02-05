32196b84fd3c1bf22cf6562c663c6c5945602447
paolo-della-fera <paolodf2005@gmail.com>
2026-02-05 10:44:19 +0100

    add comments

--- snack-1/main.js
+++ snack-1/main.js
@@ -2,14 +2,17 @@
 let firstNumber = parseInt(prompt("Inserisci il primo numero"));
 let secondNumber = parseInt(prompt("Inserisci il secondo numero"));
 
+// SE il primo numero è maggiore lo stampa
 if (firstNumber > secondNumber) {
     console.log('Il primo numero è più grande');
 }
 
+// ALTRIMENTI SE il secondo è maggiore viene stampato
 else if (firstNumber < secondNumber) {
     console.log('Il secondo numero è più grande');
 }
 
+// ALTRIMENTI se i numeri sono uguali 
 else {
     console.log('I due numeri sono uguali');
 }
